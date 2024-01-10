56fd66b93c2ff77a281dbad752c4236029864407
Charlie Newlinds <cnewlinds@gmail.com>
2024-01-10 11:48:39 +0700

    sytax added

---
+++ lib/migrations/versions/4db6953624d0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4db6953624d0
+Revises: 
+Create Date: 2024-01-10 11:36:51.341493
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4db6953624d0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
